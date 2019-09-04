0e02614b1e4c40f3e852750aa1ed69bd7166a382
pioctor <pioctorcasot@gmail.com>
2019-09-04 14:35:22 +0900

    add document class

--- src/Document.ts
+++ src/Document.ts
@@ -8,3 +8,25 @@ export interface IDocument {
   getPosition2D(position: number): IntVector2;
   getPosition(position2D: IntVector2): number;
 }
+
+class Document implements IDocument {
+  size!: IntVector2;
+  text: string = "";
+  get text2D(): string[] {
+    let t = [];
+    for (let i = 0; i < this.size.y; i += this.size.x) {
+      let ty = this.text.slice(i, i + this.size.x - 1);
+      t.push(ty);
+    }
+    return t;
+  }
+  isOutOfDocument(position: number): boolean {
+    return position < 0 || this.size.x * this.size.y <= position;
+  }
+  getPosition2D(position: number): IntVector2 {
+    return new IntVector2(position % this.size.y, position / this.size.x);
+  }
+  getPosition(position2D: IntVector2): number {
+    return position2D.x + position2D.y * this.size.x;
+  }
+}
