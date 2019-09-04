0326d509a12e922695c114f33e26567ee69ac087
pioctor <pioctorcasot@gmail.com>
2019-09-04 14:37:01 +0900

    fix IntVector2 add int cast

--- src/Game.ts
+++ src/Game.ts
@@ -2,7 +2,7 @@ export class IntVector2 {
   x!: number;
   y!: number;
   constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+    this.x = Math.floor(x);
+    this.y = Math.floor(y);
   }
 }
